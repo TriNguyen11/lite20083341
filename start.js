593d4589837bcd6540ef5eeca2e85fd97c9eaa0f
TriNguyen11 <103365659+TriNguyen11@users.noreply.github.com>
2024-03-31 09:31:21 +0700

    Update start.js

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.get("/api20083341/books", async (req, res) => {
   console.info(`Getting all books`);
 
-  const books = await query("SELECT * FROM tblbook");
+  const books = await query("SELECT * FROM tblbookIUH20083341 ");
   res.json(books);
 });
 
